ddbb6b9d6d084b5487a4cf8faad5d7b1bb9b6e85
mayankpawar2458 <82704243+mayankpawar2458@users.noreply.github.com>
2022-05-08 21:41:53 +0530

    Update ffmpeg.py

--- bot/helper_funcs/ffmpeg.py
+++ bot/helper_funcs/ffmpeg.py
@@ -103,13 +103,13 @@ async def convert_video(video_file, output_directory, total_time, bot, message, 
         if difference > 0:
           ETA = TimeFormatter(difference*1000)
         percentage = math.floor(elapsed_time * 100 / total_time)
-        progress_str = "📈 <b>Progress:</b> {0}%\n[{1}{2}]".format(
+        progress_str = "𝚂𝚃𝙰𝚃𝚄𝚂: 🟢:</b> {0}%\n[{1}{2}]".format(
             round(percentage, 2),
             ''.join([FINISHED_PROGRESS_STR for i in range(math.floor(percentage / 10))]),
             ''.join([UN_FINISHED_PROGRESS_STR for i in range(10 - math.floor(percentage / 10))])
             )
-        stats = f'🗳 <b>ENCODING IN PROGRESS</b>\n\n' \
-                f'⌚ <b>TIME LEFT:</b> {ETA}\n\n' \
+        stats = f'ᴇɴᴄᴏᴅɪɴɢ ɪɴ ᴘʀᴏɢʀᴇss</b>\n\n' \
+                f'ᴇɴᴄᴏᴅɪɴɢ ᴠɪᴅᴇᴏ:</b> {ETA}\n\n' \
                 f'{progress_str}\n'
         try:
           await message.edit_text(
@@ -117,7 +117,7 @@ async def convert_video(video_file, output_directory, total_time, bot, message, 
             reply_markup=InlineKeyboardMarkup(
                 [
                     [ 
-                        InlineKeyboardButton('❌ Cancel ❌', callback_data='fuckingdo') # Nice Call 🤭
+                        InlineKeyboardButton(' Cancel ', callback_data='fuckingdo') # Nice Call 🤭
                     ]
                 ]
             )
